f4e9a47ed47ef7a91f59cdd46bbb73bff45a79a7
Katlyn <kw17611@email.vccs.edu>
2024-11-15 16:03:25 +0000

    code

--- Ch5Ex1.cpp
+++ Ch5Ex1.cpp
@@ -1,46 +1,84 @@
-// get the sum of all entered numbers
+// hotel occupancy
 
 #include <iostream>
 using namespace std;
 
-int GetUserNumber();
-int GenerateSum(int number);
-void DisplaySum();
+int GetFloors();
+int GetRoomsFloor(int floorNumber);
+int GetOccupied(int floorNumber, int numRooms);
+//int GetTotalRooms(int floorNumber, int unoccupied);
+void DisplayResults(int rooms, int unoccupied);
 
-
+const int minFloors = 15;
+const int minRoomsPF = 10;
 
 int main(){
+int numberOfFloors = GetFloors();
+  
+  int totalRooms = 0;
+  int totalOccupiedRooms = 0;
+  for (int floorNumber = 1; floorNumber <= numberOfFloors; numberOfFloors++){
+ if (floorNumber==13)
+   continue;
+    else{
+    totalRooms += GetRoomsFloor(floorNumber);
+    totalOccupiedRooms += GetOccupied(floorNumber, totalRooms);}
+}
+  
 
-  GetUserNumber();
-  GenerateSum(GetUserNumber());
-  DisplaySum(int topNumber, int sum);
   
 }
 
-int GetUserNumber(){
-int number;
-  cout << "Please enter the top number for the sum." << endl;
-cin >> number;
-  return number;
 
-  while (number < 0){
-    cout << "Please enter a positive number." << endl;
-     cout << "Please enter the top number for the sum." << endl;
-    cin >> number;
-    
+
+int GetFloors(){
+int floors;
+  cout << "Please enter the number of floors." << endl;
+  cin >> floors;
+  
+while (floors < minFloors){
+  cout << "Must be at least " << minFloors << " floors." << endl;
+  cout << "Please enter again." << endl;
+  cin >> floors;
+}
+  return floors;
+}
+
+int GetRoomsFloor(int floorNumber){
+  int rooms;
+    cout << "Please enter the number of rooms on floor #: "<< floorNumber << endl;
+    cin >> rooms;
+
+  while (rooms < minRoomsPF){
+    cout << "Must be at least " << minRoomsPF << " rooms." << endl;
+    cout << "Please enter again." << endl;
+    cin >> rooms;
   }
+    return rooms;
+
+  
 }
 
-int GenerateSum(int topNumber){
-int sum = 0;
-  for (int num = 1; num <= topNumber; num++){
- sum += num;
-  return sum;}
+int GetOccupied(int floorNumber, int numberOfRooms, int numRooms){
+
+  
+    cout << "Please enter the number of occupied rooms on floor #: "<< floorNumber << endl;
+    cin >> numRooms;
+
+  while (numRooms > numberOfRooms){
+    cout << "You can't have more than " << numberOfRooms << " rooms." << endl;
+    cout << "Please enter the number of occupied rooms." << endl;
+    cin >> numRooms;
+  }
+    return numRooms;
+
+
 }
 
+void DisplayResults(int totalRooms, int totalOccupiedRooms){
+cout << totalRooms << endl;
+cout << totalOccupiedRooms << endl;
+cout << totalRooms/totalOccupiedRooms << endl;
 
-void DisplaySum(int topNumber,int sum){
-cout << "The sum from 1 to " << topNumber << " is " << sum << endl;  
+  
 }
-
-
