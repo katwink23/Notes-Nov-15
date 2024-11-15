34ef6e1928806de76fb5bf3eb2fa90b8e05ee6ee
Katlyn <kw17611@email.vccs.edu>
2024-11-15 15:05:36 +0000

    code

--- Ch5Ex1.cpp
+++ Ch5Ex1.cpp
@@ -1,11 +1,46 @@
-
+// get the sum of all entered numbers
 
 #include <iostream>
 using namespace std;
 
+int GetUserNumber();
+int GenerateSum(int number);
+void DisplaySum();
 
 
 
 int main(){
+
+  GetUserNumber();
+  GenerateSum(GetUserNumber());
+  DisplaySum();
   
 }
+
+int GetUserNumber(){
+int number;
+  cout << "Please enter the top number for the sum." << endl;
+cin >> number;
+  return number;
+
+  while (number < 0){
+    cout << "Please enter a positive number." << endl;
+     cout << "Please enter the top number for the sum." << endl;
+    cin >> number;
+    
+  }
+}
+
+int GenerateSum(int topNumber){
+int sum = 0;
+  for (int num = 1; num <= topNumber; num++){
+ sum += num;
+  return sum;}
+}
+
+
+void DisplaySum(int topNumber,int sum){
+cout << "The sum from 1 to " << topNumber << " is " << sum << endl;  
+}
+
+
